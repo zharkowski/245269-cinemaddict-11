4b6dffdaa219113fd7e35490d7317291ec788ce5
zharkowski <maxim199919@rambler.ru>
2020-05-20 21:36:12 +0300

    добавляет модели фильмов метод updateFilm

--- src/models/films.js
+++ src/models/films.js
@@ -10,4 +10,15 @@ export default class Film {
   set films(films) {
     this._films = films;
   }
+
+  updateFilm(id, updatedFilm) {
+    const index = this._films.findIndex((it) => it.id === id);
+    if (index === -1) {
+      return false;
+    }
+
+    this._films = [].concat(this._films.slice(0, index), updatedFilm, this._films.slice(index + 1));
+
+    return true;
+  }
 }
