c0ec9e294cbb80f40eb7e27bdc3744503caee33a
zharkowski <maxim199919@rambler.ru>
2020-05-21 03:01:06 +0300

    добавляет модели фильмов методы добавления обработчиков

--- src/models/films.js
+++ src/models/films.js
@@ -4,6 +4,13 @@ export default class Films {
   constructor() {
     this._films = null;
     this._activeFilterType = null;
+
+    this._filterChangeHandlers = [];
+    this._dataChangeHandlers = [];
+  }
+
+  _callHandlers(handlers) {
+    handlers.forEach((handler) => handler());
   }
 
   get films() {
@@ -15,7 +22,21 @@ export default class Films {
   }
 
   set allFilms(films) {
-    this._films = films;
+    this._films = Array.from(films);
+    this._callHandlers(this._dataChangeHandlers);
+  }
+
+  setFilter(filterType) {
+    this._activeFilterType = filterType;
+    this._callHandlers(this._dataChangeHandlers);
+  }
+
+  setFilterChangeHandler(handler) {
+    this._filterChangeHandlers.push(handler);
+  }
+
+  setDateChangeHandler(handler) {
+    this._dataChangeHandlers.push(handler);
   }
 
   updateFilm(id, updatedFilm) {
@@ -25,6 +46,7 @@ export default class Films {
     }
 
     this.allFilms = [].concat(this.allFilms.slice(0, index), updatedFilm, this.allFilms.slice(index + 1));
+    this._callHandlers(this._dataChangeHandlers);
 
     return true;
   }
