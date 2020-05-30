dc8535976f383fa8c673ccfe0b5023cd94d427b3
zharkowski <maxim199919@rambler.ru>
2020-05-31 00:58:46 +0300

    фиксит переключение фильтра статистики

--- src/components/statistic.js
+++ src/components/statistic.js
@@ -137,9 +137,9 @@ const createStatisticTemplate = (films, rangeType) => {
   const alreadyWatchedFilms = getAlreadyWatchedFilms(films);
   const watchedInRangeFilms = getFilmsByDateRange(alreadyWatchedFilms, rangeType);
   const alreadyWatchedFilmsCount = watchedInRangeFilms.length;
-  const [durationHours, durationMinutes] = getTotalDuration(watchedInRangeFilms);
+  const [durationHours, durationMinutes] = alreadyWatchedFilmsCount === 0 ? [0, 0] : getTotalDuration(watchedInRangeFilms);
   const genres = getGenres(watchedInRangeFilms);
-  const topGenre = genres[0].genre;
+  const topGenre = alreadyWatchedFilmsCount === 0 ? `` : genres[0].genre;
 
   return (
     `<section class="statistic">
@@ -152,26 +152,26 @@ const createStatisticTemplate = (films, rangeType) => {
       <form action="https://echo.htmlacademy.ru/" method="get" class="statistic__filters">
         <p class="statistic__filters-description">Show stats:</p>
 
-        <input type="radio" class="statistic__filters-input visually-hidden" name="statistic-filter" id="statistic-all-time" value="all-time" checked>
+        <input type="radio" class="statistic__filters-input visually-hidden" name="statistic-filter" id="statistic-all-time" value="all-time" ${rangeType === RangeType.ALL_TIME ? `checked` : ``}>
         <label for="statistic-all-time" class="statistic__filters-label">All time</label>
 
-        <input type="radio" class="statistic__filters-input visually-hidden" name="statistic-filter" id="statistic-today" value="today">
+        <input type="radio" class="statistic__filters-input visually-hidden" name="statistic-filter" id="statistic-today" value="today" ${rangeType === RangeType.TODAY ? `checked` : ``}>
         <label for="statistic-today" class="statistic__filters-label">Today</label>
 
-        <input type="radio" class="statistic__filters-input visually-hidden" name="statistic-filter" id="statistic-week" value="week">
+        <input type="radio" class="statistic__filters-input visually-hidden" name="statistic-filter" id="statistic-week" value="week" ${rangeType === RangeType.WEEK ? `checked` : ``}>
         <label for="statistic-week" class="statistic__filters-label">Week</label>
 
-        <input type="radio" class="statistic__filters-input visually-hidden" name="statistic-filter" id="statistic-month" value="month">
+        <input type="radio" class="statistic__filters-input visually-hidden" name="statistic-filter" id="statistic-month" value="month" ${rangeType === RangeType.MONTH ? `checked` : ``}>
         <label for="statistic-month" class="statistic__filters-label">Month</label>
 
-        <input type="radio" class="statistic__filters-input visually-hidden" name="statistic-filter" id="statistic-year" value="year">
+        <input type="radio" class="statistic__filters-input visually-hidden" name="statistic-filter" id="statistic-year" value="year" ${rangeType === RangeType.YEAR ? `checked` : ``}>
         <label for="statistic-year" class="statistic__filters-label">Year</label>
       </form>
 
       <ul class="statistic__text-list">
         <li class="statistic__text-item">
           <h4 class="statistic__item-title">You watched</h4>
-          <p class="statistic__item-text">${alreadyWatchedFilmsCount} <span class="statistic__item-description">movie${alreadyWatchedFilmsCount === 1 ? `:` : `s`}</span></p>
+          <p class="statistic__item-text">${alreadyWatchedFilmsCount} <span class="statistic__item-description">movie${alreadyWatchedFilmsCount === 1 ? `` : `s`}</span></p>
         </li>
         <li class="statistic__text-item">
           <h4 class="statistic__item-title">Total duration</h4>
@@ -201,7 +201,10 @@ export default class Statistic extends AbstractSmartComponent {
 
     this.setRangeChangeHandler();
   }
-  recoveryListeners() {}
+
+  recoveryListeners() {
+    this.setRangeChangeHandler();
+  }
 
   getTemplate() {
     return createStatisticTemplate(this._films, this._rangeType);
@@ -225,7 +228,6 @@ export default class Statistic extends AbstractSmartComponent {
   rerender(films, rangeType) {
     this._films = films;
     this._rangeType = rangeType;
-
     super.rerender();
 
     this._renderChart();
@@ -237,10 +239,10 @@ export default class Statistic extends AbstractSmartComponent {
   }
 
   setRangeChangeHandler() {
-    const formElement = this.getElement().querySelector(`.statistic__filters`);
-    formElement.addEventListener(`change`, () => {
-      const rangeType = formElement.elements[`statistic-filter`].value;
+    const inputElements = this.getElement().querySelectorAll(`.statistic__filters-input`);
+    inputElements.forEach((input) => input.addEventListener(`click`, (evt) => {
+      const rangeType = evt.target.value;
       this.rerender(this._films, rangeType);
-    });
+    }));
   }
 }
